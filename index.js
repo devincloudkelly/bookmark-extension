35a8ba20f72601f3460fe1677748f5437511e2c8
devincloudkelly <devin.cloud.kelly@gmail.com>
2022-02-09 22:24:42 -0600

    Clean up notes

--- index.js
+++ index.js
@@ -44,26 +44,14 @@ function addNoteListeners() {
 
 function addNotes(e) {
   const btnId = e.target.id;
+  // get the number at end of button id from click event.
   const index = parseInt(btnId.slice(-1));
+  // use that number to match the button to the element index in Bookmarks
   console.log(
     `adding a note to the ${index} index of the bookmarks tab, `,
     bookmarks[index]
   );
+  // add the new note as the second element in the matching array. ex, element one in bookmarks is an array with a url in the first position; this note will go in the second position of that array,
 }
-// declare allNotesBtns below wince they are only rendered after bookmarks are rendered.
 
-// iterate over each button node, add an event listener to listen for clicks.
-
-// allNotesBtns.forEach((btn, index) => {
-//   btn.addEventListener("click", (e) => {
-//     addNotes(e);
-//   });
-// });
-
-// get the number at end of button id from click event.
-// use that number to match the button to the element index in Bookmarks
 // update bookmarks above so that every new bookmark is saved as an array instead of a string.
-// add the new note as the second element in the matching array. ex, element one in bookmarks is an array with a url in the first position; this note will go in the second position of that array,
-
-// To work on next - when I clear my buttons, then recreate, the addNotes function isn't running.
-// Do I need to remove the 'allNotesBtns and the iteration after and build that into the render bookmarks function?
